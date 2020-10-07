d8d8f3d8645fbbe5ae492c527c912ca3a7a2ad60
boogy <0xboogy@gmail.com>
2020-10-07 17:17:30 +0200

    bpswm update

---
+++ .config/scripts/xinput-config.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+xinput --set-prop "DLL06E4:01 06CB:7A13 Touchpad" "libinput Natural Scrolling Enabled" 0
+xinput --set-prop "DLL06E4:01 06CB:7A13 Touchpad" "libinput Click Method Enabled" 0 1
+xinput --set-prop "DLL06E4:01 06CB:7A13 Touchpad" 'libinput Accel Speed' 0.6
+xinput --set-prop "Logitech Perforance MX" "Device Accel Constant Deceleration" 0.700000
+xinput --set-prop "ZLY ZELOTES GAME MOUSE" "Device Accel Constant Deceleration" 0.700000
+xinput --set-prop "pointer:Logitech MX Master 2S" "libinput Accel Speed" 1.000000
+xinput --set-prop "SynPS/2 Synaptics TouchPad" "libinput Accel Speed" 1
+xinput --set-prop "pointer:Logitech MX Anywhere 2" "libinput Accel Speed" 1
+xinput --set-prop "Logitech G502 HERO Gaming Mouse" "libinput Accel Speed" 1
