24b103025d56c070f6fd7211f89a7f5c428c4710
boogy <0xboogy@gmail.com>
2020-11-02 17:29:22 +0100

    picom conf update, user.js zoom profiles

--- deploy/conf/firefox/user.js
+++ deploy/conf/firefox/user.js
@@ -70,3 +70,4 @@ user_pref("devtools.chrome.enabled", false);
 user_pref("devtools.debugger.force-local", true);
 
 user_pref("extensions.pocket.enabled", false);
+user_pref("toolkit.zoomManager.zoomValues", ".3,.5,.67,.8,.9,1,1.1,1.2,1.25,1.3,1.4,1.5,1.7,2,2.4,3")
