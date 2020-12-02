bee4abc044c0b98a189e52e00ff2f7b24c890d81
boogy <0xboogy@gmail.com>
2020-12-02 14:14:28 +0100

    sxhkd update + paru alias + picom backend

--- .bash/aliases.bash
+++ .bash/aliases.bash
@@ -173,7 +173,7 @@ alias cd-="cd -"
     ## alternative to yay with some cool options
     ## option can be changed in /etc/paru.conf
     alias paru="paru --bottomup"
-    alias pupdate="paru -Syyu --bottomup --cleanafter --nocombinedupgrade --useask"
+    alias pupdate="paru -Syyu --bottomup --cleanafter --nocombinedupgrade --useask --upgrademenu"
     alias aur-show="paru -Gp"
     alias aur-download="paru -G"
     alias pinstall="paru"
