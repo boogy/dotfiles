a0e638786ca3d6a3f1a34e391d89a9c07c94c3fe
boogy <0xboogy@gmail.com>
2020-12-29 16:34:59 +0100

    xmonad + polybar + bolt updates

--- .config/polybar/launch-polybar.sh
+++ .config/polybar/launch-polybar.sh
@@ -16,7 +16,7 @@ if [ -z "$POLYBAR_PRIMARY_MONITOR" ]; then
     export POLYBAR_PRIMARY_MONITOR=$(xrandr | grep primary | cut -d ' ' -f 1)
 fi
 
-## set DPI value
+## set polybar DPI value
 AUTORANDR_CONFIG=$(autorandr --detect)
 case $AUTORANDR_CONFIG in
     home*)
@@ -32,28 +32,28 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
 outputs=$(xrandr --listactivemonitors|awk '{print $4}'|sed '/^$/d')
 tray_output=${POLYBAR_PRIMARY_MONITOR}
 
+_BAR_NAME=""
 case $DESKTOP_SESSION in
     bspwm)
-        for m in $outputs; do
-            export MONITOR=$m
-            export TRAY_POSITION=none
-            if [[ ${m} == ${tray_output} ]]; then
-                TRAY_POSITION=right
-            fi
-            MONITOR=$m polybar --reload bar-bspwm -c ~/.config/polybar/config &
-        done
+        _BAR_NAME="bar-bspwm"
         ;;
     i3)
-        polybar --reload bar-i3 -c ~/.config/polybar/config &
+        _BAR_NAME="bar-i3"
         ;;
     xmonad)
-        for m in $outputs; do
-            export MONITOR=$m
-            export TRAY_POSITION=none
-            if [[ ${m} == ${tray_output} ]]; then
-                TRAY_POSITION=right
-            fi
-            MONITOR=$m polybar --reload bar-xmonad -c ~/.config/polybar/config &
-        done
+        _BAR_NAME="bar-xmonad"
         ;;
+    *)
+        _BAR_NAME="bar-default"
 esac
+
+## run a polybar instance per screen
+for m in $outputs; do
+    export MONITOR=$m
+    export TRAY_POSITION=none
+
+    if [[ ${m} == ${tray_output} ]]; then
+        TRAY_POSITION=right
+    fi
+    MONITOR=$m polybar --reload "$_BAR_NAME" -c ~/.config/polybar/config &
+done
