1eedb674fa566a3babc7f58c6b294fc82b133c8f
boogy <boogy@rootsec.ch>
2022-12-30 03:03:51 +0100

    update

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -3,6 +3,50 @@
 require('indent_blankline').setup {
   char = '┊',
   show_trailing_blankline_indent = false,
+
 }
 
+vim.opt.list = true
+-- vim.opt.listchars:append "space:⋅"
+-- vim.opt.listchars:append "eol:↴"
 
+vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
+
+vim.g.indent_blankline_filetype_exclude = {
+	"help",
+	"startify",
+	"dashboard",
+	"packer",
+	"neogitstatus",
+	"NvimTree",
+	"Trouble",
+}
+
+vim.g.indent_blankline_context_patterns = {
+	"class",
+	"return",
+	"function",
+	"method",
+	"^if",
+	"^while",
+	"jsx_element",
+	"^for",
+	"^object",
+	"^table",
+	"block",
+	"arguments",
+	"if_statement",
+	"else_clause",
+	"jsx_element",
+	"jsx_self_closing_element",
+	"try_statement",
+	"catch_clause",
+	"import_statement",
+	"operation_type",
+}
+
+require("indent_blankline").setup {
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+}
