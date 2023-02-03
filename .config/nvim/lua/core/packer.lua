65a2bebf74816fcd4b199df4d3233bc2699149e7
boogy <boogy@rootsec.ch>
2023-02-03 09:11:03 +0100

    update

--- .config/nvim/lua/core/packer.lua
+++ .config/nvim/lua/core/packer.lua
@@ -34,7 +34,7 @@ require('packer').startup({ function(use)
       'saadparwaiz1/cmp_luasnip',
       -- additional autocompletions
       'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
+      -- 'hrsh7th/cmp-cmdline',
       'hrsh7th/cmp-buffer',
     },
   }
