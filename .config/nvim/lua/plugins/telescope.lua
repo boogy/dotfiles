7ae8890bbcd41958222320c040627b3145f9927c
boogy <boogy@rootsec.ch>
2023-04-28 11:22:10 +0200

    update

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,19 +1,38 @@
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
+local actions = require("telescope.actions")
+local action_layout = require("telescope.actions.layout")
+
 require('telescope').setup {
   defaults = {
     mappings = {
+      n = {
+        ["<C-i>"] = action_layout.toggle_preview,
+      },
       i = {
         ['<C-u>'] = false,
-        ['<C-d>'] = false,
+        ['<C-d>'] = actions.delete_buffer + actions.move_to_top, -- ['<C-d>'] = false
+        ["<esc>"] = actions.close,
+        ["<C-i>"] = action_layout.toggle_preview
       },
     },
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--trim" -- add this value
+    }
   },
   pickers = {
     find_files = {
       find_command = { "fd", "--type", "f", "--strip-cwd-prefix" },
+      -- find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
       -- layout_strategy='vertical',
-      layout_config={width=0.8}
+      layout_config = { width = 0.8 },
     },
   },
   file_ignore_patterns = { "node%_modules/.*" }
@@ -45,8 +64,13 @@ keymap.set('n', '<leader>bk', require('telescope.builtin').keymaps, { desc = 'Bu
 
 
 -- telescope git commands (not on youtube nvim video)
-keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>", { desc = 'List all git commits (use <cr> to checkout) ["gc" for git commits]'})
-keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>", { desc = 'List git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]' })
-keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>", {desc = 'List git branches (use <cr> to checkout) ["gb" for git branch]'})
-keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>", {desc = 'List current changes per file with diff preview'})
-keymap.set("n", "<leader>gS", "<cmd>Telescope git_stash<cr>", {desc = 'Lists stash items in current repository with ability to apply them on <cr>'})
+keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>",
+  { desc = 'List all git commits (use <cr> to checkout) ["gc" for git commits]' })
+keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>",
+  { desc = 'List git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]' })
+keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>",
+  { desc = 'List git branches (use <cr> to checkout) ["gb" for git branch]' })
+keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>",
+  { desc = 'List current changes per file with diff preview' })
+keymap.set("n", "<leader>gS", "<cmd>Telescope git_stash<cr>",
+  { desc = 'Lists stash items in current repository with ability to apply them on <cr>' })
