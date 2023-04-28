8ff6413d51aec0a4c91f29beb42f574793f063f1
boogy <boogy@rootsec.ch>
2023-04-28 18:13:31 +0200

    update

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -169,10 +169,29 @@ keymap.set("n", "<leader>gS", "<cmd>Telescope git_stash<cr>",
 -- Harpoon
 --
 --------------------------------------------------------------------------------------------------
-keymap.set("n", "<leader>hp", require('harpoon.ui').toggle_quick_menu)
-keymap.set("n", "<leader>hm", require('harpoon.mark').add_file)
-keymap.set("n", "<M-n>", require('harpoon.ui').nav_next)
-keymap.set("n", "<M-p>", require('harpoon.ui').nav_prev)
+keymap.set("n", "<C-e>", require('harpoon.ui').toggle_quick_menu)
+
+keymap.set("n", "<leader>a", require('harpoon.mark').add_file)
+-- keymap.set("n", "<M-a>", function() require('harpoon.ui').nav_file(1) end )
+-- keymap.set("n", "<M-s>", function() require('harpoon.ui').nav_file(2) end )
+-- keymap.set("n", "<M-d>", function() require('harpoon.ui').nav_file(3) end )
+
+keymap.set("n", "<M-1>", function() require('harpoon.ui').nav_file(1) end )
+keymap.set("n", "<M-2>", function() require('harpoon.ui').nav_file(2) end )
+keymap.set("n", "<M-3>", function() require('harpoon.ui').nav_file(3) end )
+keymap.set("n", "<M-4>", function() require('harpoon.ui').nav_file(4) end )
+keymap.set("n", "<M-5>", function() require('harpoon.ui').nav_file(5) end )
+keymap.set("n", "<M-6>", function() require('harpoon.ui').nav_file(6) end )
+keymap.set("n", "<M-7>", function() require('harpoon.ui').nav_file(7) end )
+keymap.set("n", "<M-8>", function() require('harpoon.ui').nav_file(8) end )
+keymap.set("n", "<M-9>", function() require('harpoon.ui').nav_file(9) end )
+
+-- keymap.set("n", "<leader>1", ":call v:lua.require('harpoon.ui').nav_file(1)<CR>")
+-- keymap.set("n", "<leader>2", ":call v:lua.require('harpoon.ui').nav_file(2)<CR>")
+-- keymap.set("n", "<leader>3", ":call v:lua.require('harpoon.ui').nav_file(3)<CR>")
+
+-- keymap.set("n", "<M-n>", require('harpoon.ui').nav_next)
+-- keymap.set("n", "<M-p>", require('harpoon.ui').nav_prev)
 
 
 -------------------------------------------------------------------------------------------
