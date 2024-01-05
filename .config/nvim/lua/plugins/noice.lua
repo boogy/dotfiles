50e239f3d7500b44e2f69b61dfd6801d978fb36a
boogy <boogy@rootsec.ch>
2024-01-05 10:20:10 +0100

    update

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,30 @@
+return {
+  "folke/noice.nvim",
+  --
+  -- delete default options from LazyVim
+  --
+  opts = function()
+    return {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+            },
+          },
+          view = "mini",
+        },
+      },
+    }
+  end,
+}
