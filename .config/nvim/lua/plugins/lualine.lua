81a353f5b61fc418afbc717f1afa51a8dbe4140d
boogy <boogy@rootsec.ch>
2025-01-08 10:38:32 +0100

    update

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -48,24 +48,25 @@ return {
 
         lualine_x = {
           -- stylua: ignore
-          {
-            function() return require("noice").api.status.command.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
-            color = Util.ui.fg("Statement"),
-          },
+          -- {
+          --   function() return require("noice").api.status.command.get() end,
+          --   cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+          --   -- color = Snaks.util.color("Statement"),
+          --   -- color = Snaks.Util.color("Statement")
+          -- },
           -- stylua: ignore
-          {
-            function() return require("noice").api.status.mode.get() end,
-            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
-            color = Util.ui.fg("Constant"),
-          },
+          -- {
+          --   function() return require("noice").api.status.mode.get() end,
+          --   cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+          --   -- color = Util.ui.fg("Constant"),
+          -- },
           -- stylua: ignore
           {
             function() return "  " .. require("dap").status() end,
             cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
-            color = Util.ui.fg("Debug"),
+            -- color = Util.ui.fg("Debug"),
           },
-          { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = Util.ui.fg("Special") },
+          -- { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = Util.ui.fg("Special") },
           {
             "diff",
             symbols = {
